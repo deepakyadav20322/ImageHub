d080409ce7537bd7e728531245064506f7c08b46
deepak <informationtechnology20322@gmail.com>
2025-03-10 15:38:51 +0530

    Initialize react project with shadcn ui

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Button } from '@/components/ui/button'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     Hello , React project for Image assets management system
+     <Button>Click me</Button>
+
+    </>
+  )
+}
+
+export default App
