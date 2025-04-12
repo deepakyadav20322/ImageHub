52d43083f33947f519fed1055714be21993b4c41
deepak <informationtechnology20322@gmail.com>
2025-04-12 22:42:46 +0530

    .

--- frontend/src/components/accountSettingComponents/tabs/user-management-tab.tsx
+++ frontend/src/components/accountSettingComponents/tabs/user-management-tab.tsx
@@ -8,7 +8,7 @@ import {
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+} from"@/components/ui/dropdown-menu"
 import { Input } from "@/components/ui/input"
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
 import { MoreHorizontal, Plus, Search, User } from "lucide-react"
